74023269478ad14508c48a0df2c70e0b529316ee
Eren AVAR <eren.avar@hotmail.com>
2024-07-20 16:06:07 +0300

    add login inputs into the auth form

--- src/components/AuthForm/AuthForm.jsx
+++ src/components/AuthForm/AuthForm.jsx
@@ -1,7 +1,18 @@
+import { Box, Image, Input, VStack } from "@chakra-ui/react";
 import React from "react";
 
 const AuthForm = () => {
-  return <div>AuthForm</div>;
+  return (
+    <>
+      <Box border={"1px solid gray"} borderRadius={4} padding={5}>
+        <VStack spacing={4}>
+          <Image src="./logo.png" h={24} cursor={"pointer"} />
+          <Input placeholder="Email" fontSize={14} type="email" />
+          <Input placeholder="Password" fontSize={14} type="password" />
+        </VStack>
+      </Box>
+    </>
+  );
 };
 
 export default AuthForm;
