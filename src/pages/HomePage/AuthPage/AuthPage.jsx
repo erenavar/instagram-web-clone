68a3bf0509205228569c476f2ff64d8a779bf9cf
Eren AVAR <eren.avar@hotmail.com>
2024-07-20 12:45:48 +0300

    add auth page image and create auth form

--- src/pages/HomePage/AuthPage/AuthPage.jsx
+++ src/pages/HomePage/AuthPage/AuthPage.jsx
@@ -1,11 +1,17 @@
-import { Container, Flex } from "@chakra-ui/react";
+import { Box, Container, Flex, VStack, Image } from "@chakra-ui/react";
 import React from "react";
+import AuthForm from "../../../components/AuthForm/AuthForm";
 
 const AuthPage = () => {
   return (
     <Flex minH={"100vh"} justifyContent={"center"} alignItems={"center"} px={4}>
       <Container maxH={"container.md"} padding={0}>
-        This is some content...
+        <Box display={{ base: "none", md: "block" }}>
+          <Image src="./auth.png" h={650} alt="Phone img" />
+        </Box>
+        <VStack spacing={4} align={"stretch"}>
+          <AuthForm />
+        </VStack>
       </Container>
     </Flex>
   );
