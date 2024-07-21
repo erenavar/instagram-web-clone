da879a4a9296ec438fe2013d97bed703fddf3d8e
Eren AVAR <eren.avar@hotmail.com>
2024-07-21 12:21:17 +0300

    add pagelayout css settings

--- src/Layouts/PageLayout/PageLayout.jsx
+++ src/Layouts/PageLayout/PageLayout.jsx
@@ -1,14 +1,21 @@
-import { Box, Flex } from "@chakra-ui/react";
+import { Box, calc, Flex } from "@chakra-ui/react";
 import React from "react";
 import SideBar from "../../components/SideBar/SideBar";
+import { useLocation } from "react-router-dom";
 
-const PageLayout = () => {
+const PageLayout = ({ children }) => {
+  const { pathname } = useLocation();
   return (
     <Flex>
-      <Box>
-        <SideBar />
+      {pathname !== "/auth" ? (
+        <Box w={{ base: "70px", md: "240px" }}>
+          <SideBar />
+        </Box>
+      ) : null}
+
+      <Box flex={1} width={{ base: "calc(100%-70px)", md: "calc(100%-240px)" }}>
+        {children}
       </Box>
-      <Box></Box>
     </Flex>
   );
 };
