b5ca701c3959c96a60e026caf25b262de11364ca
Eren AVAR <eren.avar@hotmail.com>
2024-07-21 19:16:20 +0300

    code sidebar component

--- src/components/SideBar/SideBar.jsx
+++ src/components/SideBar/SideBar.jsx
@@ -1,7 +1,31 @@
-import React from "react";
+import { Box, Flex, Link } from "@chakra-ui/react";
+import { Link as RouterLink } from "react-router-dom";
 
 const SideBar = () => {
-  return <div>SideBar</div>;
+  return (
+    <Box
+      height={"100vh"}
+      borderRight={"1px solid"}
+      borderColor={"whiteAlpha.300"}
+      py={8}
+      position={"sticky"}
+      top={0}
+      left={0}
+      px={{ base: 2, md: 4 }}
+    >
+      <Flex flexDirection={"column"} gap={"10"} w={"full"} height={"full"}>
+        <Link
+          to={"/"}
+          as={RouterLink}
+          pl={2}
+          display={{ base: "none", md: "block" }}
+          cursor="pointer"
+        >
+          <InstagramLogo />
+        </Link>
+      </Flex>
+    </Box>
+  );
 };
 
 export default SideBar;
